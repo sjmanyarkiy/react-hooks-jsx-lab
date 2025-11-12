11b9be329b1b8f68be57c6fc875718de31c8ef8a
commit-panda <sjmanyarkiy@gmail.com>
2025-11-12 18:14:07 +0300

    complete lab

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,15 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  const style = {
+    color: "firebrick"
+  }
+  return (
+  <div id="home">
+    <h1 style={ style }>{ name } is a Web Developer from { city }</h1>
+  </div>
+
+)
 }
 
 export default Home;
